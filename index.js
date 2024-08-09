9da9fbc5ce60b182712ddef7a91dc05536ae5050
Ashish Kushwaha <ashhome918@gmail.com>
2024-08-09 11:13:50 +0530

    first react app

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent=React.createElement("div",{},[
-    React.createElement("div",{id:"child1"}
+    React.createElement("div",{id:"child1"},
         [
             React.createElement("h1",{},"This is h1 tag"),
             React.createElement("h2",{},"This is h2 tag")
         ]
      ),
-     React.createElement("div",{id:"child2"}
+     React.createElement("div",{id:"child2"},
         [
             React.createElement("h1",{},"This is h1 tag"),
             React.createElement("h2",{},"This is h2 tag")
