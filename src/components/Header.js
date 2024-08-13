dc0b51f68f8cb2799ab12601ef55f790d9f431c1
Ashish Kushwaha <ashhome918@gmail.com>
2024-08-13 14:03:44 +0530

    Shimmer and UseState

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
-const Header= () => (
-    <div className="header">
+const Header= () => {
+    let s=1;
+    const[btnName,setBtnName]=useState("Login");
+ return <div className="header">
         <div className="logo-container">
             <img alt="logo" className="logo"
             src={LOGO_URL}>
@@ -12,10 +15,16 @@ const Header= () => (
                 <li>Home</li>
                 <li>About us</li>
                 <li>Contact us</li>
-                <li>Cart</li>
+                <li>Cart {s}</li>
+                <button className="log-btn"
+                onClick={()=>{
+                    s=5;
+                    btnName==="Login"?setBtnName("LogOut"):setBtnName("Login");
+                }}
+                >{btnName}</button>
             </ul>
         </div>
     </div>
-);
+};
 
 export default Header;
